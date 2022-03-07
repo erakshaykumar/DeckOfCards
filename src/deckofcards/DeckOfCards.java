a55c831e9a6c14af80dcd9d6d6ef6d60fbd7e65f
erakshaykumar <akaakshay304@gmail.com>
2022-03-07 08:31:24 +0530

    Added: Welcome Message For Deck Of Cards

---
+++ src/deckofcards/DeckOfCards.java
@@ -0,0 +1,17 @@
+/*@Purpose : Write a Program to initialise deck of cards having suit ("Clubs", "Diamonds", "Hearts", "Spades") & Rank ("2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace").
+ * Shuffle the cards using Random method and then distribute 9 Cards to 4 Players and Print the Cards the received by the 4 Players.
+ * 1. Ability to work with GIT
+ * 2. Expected to have OOPs
+ * 3. Expected to have Data Structures
+ * 
+ *@File : Deck Of Cards
+ *@Author : Akshay Kumar
+ */
+package deckofcards;
+
+public class DeckOfCards {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Deck Of Cards Program");
+	}
+}
